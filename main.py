72ffa5da5f714116fe97d283d63bfd324398bfbe
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-03-09 22:12:20 +0800

    conv3d autoencoder ok
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def to_img(x):
     return x
  
  
-num_epochs = 100
+num_epochs = 1000
 batch_size = 128
 learning_rate = 1e-3
  
@@ -29,27 +29,27 @@ img_transform = transforms.Compose([
     transforms.Normalize((0.5,), (0.5,))
 ])
  
-dataset = MNIST('./data', transform=img_transform, download=True)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+# dataset = MNIST('./data', transform=img_transform, download=True)
+# dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
  
  
 class autoencoder(nn.Module):
     def __init__(self):
         super(autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+            nn.Conv3d(in_channels=1, out_channels=4, kernel_size=2, stride=2, padding=0),  # b, 16, 10, 10
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
-            nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
+            # nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+            nn.Conv3d(4, 1, 2, stride=2, padding=0),  # b, 8, 3, 3
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+            # nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+            nn.ConvTranspose3d(1, 4, 2, stride=2),  # b, 16, 5, 5
             nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
-            nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.ConvTranspose3d(4, 1, 2, stride=2, padding=0),  # b, 8, 15, 15
+            # nn.ReLU(True),
+            # nn.ConvTranspose3d(1, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
             nn.Tanh()
         )
  
@@ -63,22 +63,24 @@ model = autoencoder()#.cuda()
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
                              weight_decay=1e-5)
- 
+
+img = torch.randn(128, 1, 28, 28, 28)
 for epoch in range(num_epochs):
-    for data in dataloader:
-        img, _ = data
-        img = Variable(img)# .cuda()
-        # ===================forward=====================
-        output = model(img)
-        loss = criterion(output, img)
-        # ===================backward====================
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    # for data in dataloader:
+    # img, _ = data
+    # img = Variable(img)# .cuda()
+    # ===================forward=====================
+    # print(img.shape)
+    output = model(img)
+    loss = criterion(output, img)
+    # ===================backward====================
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
     # ===================log========================
     print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, loss.item()))
-    if epoch % 10 == 0:
-        pic = to_img(output.cpu().data)
-        save_image(pic, './dc_img/image_{}.png'.format(epoch))
+    # if epoch % 10 == 0:
+    #     pic = to_img(output.cpu().data)
+    #     save_image(pic, './dc_img/image_{}.png'.format(epoch))
  
 torch.save(model.state_dict(), './conv_autoencoder.pth')
