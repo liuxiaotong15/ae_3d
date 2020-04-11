863636cdb13f0e9cb2dadb5f319cc0ab53686ecd
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-04-11 16:26:32 +0800

    ddpg
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- Policy-Gradient-Methods/ddpg/ddpg.py
+++ Policy-Gradient-Methods/ddpg/ddpg.py
@@ -49,36 +49,36 @@ class DDPGAgent:
         return action
     
     def update(self, batch_size):
-        # states, actions, rewards, next_states, _ = self.replay_buffer.sample(batch_size)
-        # state_batch, action_batch, reward_batch, next_state_batch, masks = self.replay_buffer.sample(batch_size)
-        # state_batch = torch.FloatTensor(state_batch).to(self.device)
-        # action_batch = torch.FloatTensor(action_batch).to(self.device)
-        # reward_batch = torch.FloatTensor(reward_batch).to(self.device)
-        # next_state_batch = torch.FloatTensor(next_state_batch).to(self.device)
-        # masks = torch.FloatTensor(masks).to(self.device)
+        states, actions, rewards, next_states, _ = self.replay_buffer.sample(batch_size)
+        state_batch, action_batch, reward_batch, next_state_batch, masks = self.replay_buffer.sample(batch_size)
+        state_batch = torch.FloatTensor(state_batch).to(self.device)
+        action_batch = torch.FloatTensor(action_batch).to(self.device)
+        reward_batch = torch.FloatTensor(reward_batch).to(self.device)
+        next_state_batch = torch.FloatTensor(next_state_batch).to(self.device)
+        masks = torch.FloatTensor(masks).to(self.device)
    
-        # curr_Q = self.critic.forward(state_batch, action_batch)
-        # next_actions = self.actor_target.forward(next_state_batch)
-        # next_Q = self.critic_target.forward(next_state_batch, next_actions.detach())
-        # expected_Q = reward_batch + self.gamma * next_Q
-        # 
-        # # update critic
-        # q_loss = F.mse_loss(curr_Q, expected_Q.detach())
+        curr_Q = self.critic.forward(state_batch, action_batch)
+        next_actions = self.actor_target.forward(next_state_batch)
+        next_Q = self.critic_target.forward(next_state_batch, next_actions.detach())
+        expected_Q = reward_batch + self.gamma * next_Q
+        
+        # update critic
+        q_loss = F.mse_loss(curr_Q, expected_Q.detach())
 
-        # self.critic_optimizer.zero_grad()
-        # q_loss.backward() 
-        # self.critic_optimizer.step()
-        # # update actor
-        # policy_loss = -self.critic.forward(state_batch, self.actor.forward(state_batch)).mean()
-        # 
-        # self.actor_optimizer.zero_grad()
-        # policy_loss.backward()
-        # self.actor_optimizer.step()
+        self.critic_optimizer.zero_grad()
+        q_loss.backward() 
+        self.critic_optimizer.step()
+        # update actor
+        policy_loss = -self.critic.forward(state_batch, self.actor.forward(state_batch)).mean()
+        
+        self.actor_optimizer.zero_grad()
+        policy_loss.backward()
+        self.actor_optimizer.step()
 
-        # print('c_loss: ', q_loss.item(), 'a_loss: ', policy_loss.item())
-        # # update target networks 
-        # for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
-        #     target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+        print('c_loss: ', q_loss.item(), 'a_loss: ', policy_loss.item())
+        # update target networks 
+        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
+            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
        
-        # for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
-        #     target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
