44265367526366cd52c7ff42d09266c2ad5ac239
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-04-13 22:09:45 +0800

    ddpg bigger kernel of conv
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- Policy-Gradient-Methods/ddpg/models.py
+++ Policy-Gradient-Methods/ddpg/models.py
@@ -44,7 +44,7 @@ class Actor(nn.Module):
 
         self.obs_dim = obs_dim
         self.action_dim = action_dim
-
+        # test_1
         # 1, 50, 50, 50
         # self.conv3d1 = nn.Conv3d(in_channels=1, out_channels=4, kernel_size=2, stride=2, padding=0)
         # 4, 25, 25, 25
@@ -53,24 +53,37 @@ class Actor(nn.Module):
         # 4, 25, 25, 25
         # self.conv3dT2 = nn.ConvTranspose3d(4, 1, 2, stride=2, padding=0)
         # 1, 50, 50, 50
-        self.conv3d1 = nn.Conv3d(in_channels=1, out_channels=4, kernel_size=3, stride=1, padding=2)
-        # 4, 50, 50, 50
-        self.conv3d2 = nn.Conv3d(4, 2, 5, stride=5, padding=0)
-        # 2, 10, 10, 10
-        self.conv3d3 = nn.Conv3d(2, 2, 2, stride=2, padding=0)
-        self.conv3dT1 = nn.ConvTranspose3d(2, 2, 2, stride=2)
-        # 2, 10, 10, 10
-        self.conv3dT2 = nn.ConvTranspose3d(2, 1, 5, stride=5)
+        # test_2
+        # self.conv3d1 = nn.Conv3d(in_channels=1, out_channels=4, kernel_size=3, stride=1, padding=2)
+        # # 4, 50, 50, 50
+        # self.conv3d2 = nn.Conv3d(4, 2, 5, stride=5, padding=0)
+        # # 2, 10, 10, 10
+        # self.conv3d3 = nn.Conv3d(2, 2, 2, stride=2, padding=0)
+        # # 2, 5, 5, 5
+        # self.conv3dT1 = nn.ConvTranspose3d(2, 2, 2, stride=2)
+        # # 2, 10, 10, 10
+        # self.conv3dT2 = nn.ConvTranspose3d(2, 1, 5, stride=5)
         # 4, 50, 50, 50 
         # self.conv3dT3 = nn.ConvTranspose3d(4, 1, 3, stride=1, padding=2)
         # 1, 50, 50, 50
 
+        # test_3
+        self.conv3d1 = nn.Conv3d(in_channels=1, out_channels=4, kernel_size=7, stride=2, padding=5)
+        # 4, 25, 25, 25
+        self.conv3d2 = nn.Conv3d(4, 2, 7, stride=2, padding=6)
+        # 2, 13, 13, 13
+        self.conv3d3 = nn.Conv3d(2, 2, 3, stride=3, padding=4)
+        # 2, 5, 5, 5
+        self.conv3dT1 = nn.ConvTranspose3d(2, 2, 2, stride=2)
+        # 2, 10, 10, 10
+        self.conv3dT2 = nn.ConvTranspose3d(2, 1, 5, stride=5)
+        # 1, 50, 50, 50
 
     def forward(self, obs):
         # print(obs.shape)
         x = F.relu(self.conv3d1(obs))
         x = F.relu(self.conv3d2(x))
-        x = self.conv3d3(x)
+        x = F.relu(self.conv3d3(x))
         x = F.relu(self.conv3dT1(x))
         x = self.conv3dT2(x)
         # x = self.conv3dT3(x)
