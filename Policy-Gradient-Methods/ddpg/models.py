c3aa40ceba230375121c7e182cee931d49204566
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-04-15 18:49:32 +0800

    add a batch dimension
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- Policy-Gradient-Methods/ddpg/models.py
+++ Policy-Gradient-Methods/ddpg/models.py
@@ -36,28 +36,30 @@ class Critic(nn.Module):
 
     def forward(self, x, a):
         # TODO: x = x + nn.MaxPool3d(a......) to set all small value of a to zero
+        # a is 32, 1, 50, 50, 50
         a = a.detach().numpy()
-        if np.amax(a) > 1e-5:
-            a = a/np.amax(a)
-        stt_sz = a.shape[1]
-        # mask = np.zeros(a.shape, dtype=np.float32)
-        result = np.where(a == np.amax(a))
-        # find xyz in whole action
-        x1, y1, z1 = 0, 0, 0
-        # if have multi max, choose the max sum around it
-        max_sub_arr_sum = -100
-        for idx in range(0, len(result[1])):
-            i, j, k = result[1][idx], result[2][idx], result[3][idx]
-            sub_arr = a[0, max(0, i-1):min(i+2, stt_sz), max(0, j-1):min(j+2, stt_sz), max(0, k-1):min(k+2, stt_sz)]
-            if np.sum(sub_arr) < 1e-5 or sub_arr.size < 1e-5:
-                continue
-            if np.sum(sub_arr)/sub_arr.size > max_sub_arr_sum:
-                max_sub_arr_sum = np.sum(sub_arr)/sub_arr.size
-                x1, y1, z1 = i, j, k
-        for i in range(max(0, x1-1),min(x1+2, stt_sz)):
-            for j in range(max(0, y1-1),min(y1+2, stt_sz)):
-                for k in range(max(0, z1-1),min(z1+2, stt_sz)):
-                    x[0][i][j][k] += float(a[0][i][j][k])
+        stt_sz = a.shape[2]
+        for bi in range(a.shape[0]):
+            if np.amax(a[bi]) > 1e-5:
+                a[bi] = a[bi]/np.amax(a[bi])
+            # mask = np.zeros(a.shape, dtype=np.float32)
+            result = np.where(a[bi] == np.amax(a[bi]))
+            # find xyz in whole action
+            x1, y1, z1 = 0, 0, 0
+            # if have multi max, choose the max sum around it
+            max_sub_arr_sum = -100
+            for idx in range(0, len(result[1])):
+                i, j, k = result[1][idx], result[2][idx], result[3][idx]
+                sub_arr = a[bi, 0, max(0, i-1):min(i+2, stt_sz), max(0, j-1):min(j+2, stt_sz), max(0, k-1):min(k+2, stt_sz)]
+                if np.sum(sub_arr) < 1e-5 or sub_arr.size < 1e-5:
+                    continue
+                if np.sum(sub_arr)/sub_arr.size > max_sub_arr_sum:
+                    max_sub_arr_sum = np.sum(sub_arr)/sub_arr.size
+                    x1, y1, z1 = i, j, k
+            for i in range(max(0, x1-1),min(x1+2, stt_sz)):
+                for j in range(max(0, y1-1),min(y1+2, stt_sz)):
+                    for k in range(max(0, z1-1),min(z1+2, stt_sz)):
+                        x[bi][0][i][j][k] += a[bi][0][i][j][k]
                     # mask[0][i][j][k] = a[0][i][j][k]
 
         # a = torch.from_numpy(mask)
