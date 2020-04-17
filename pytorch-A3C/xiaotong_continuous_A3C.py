efadaf89fea08b1a10e47b0c36cec0722c08aebc
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-04-17 11:55:21 +0800

    A3C: action output softmaxed voxel
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- pytorch-A3C/xiaotong_continuous_A3C.py
+++ pytorch-A3C/xiaotong_continuous_A3C.py
@@ -23,8 +23,8 @@ MAX_EP_STEP = 200
 env = gym.make('Pendulum-v0')
 N_S = env.observation_space.shape[0] # useless
 N_A = env.action_space.shape[0]
-HIGH_A = env.action_space.high[0]
-LOW_A = env.action_space.low[0]
+HIGH_A = 1
+LOW_A = 0
 
 class Net(nn.Module):
     def __init__(self, s_dim, a_dim):
@@ -44,36 +44,48 @@ class Net(nn.Module):
         # self.conv3d3 = nn.Conv3d(2, 2, 2, stride=2, padding=0)
 
         # test_3
-        self.conv3d1 = nn.Conv3d(in_channels=1, out_channels=4, kernel_size=7, stride=2, padding=3)
+        # self.conv3d1 = nn.Conv3d(in_channels=1, out_channels=4, kernel_size=7, stride=2, padding=3)
+        # # 4, 25, 25, 25
+        # self.conv3d2 = nn.Conv3d(4, 2, 7, stride=2, padding=3)
+        # # 2, 13, 13, 13
+        # self.conv3d3 = nn.Conv3d(2, 2, 3, stride=3, padding=2)
+
+        # self.fltt = 2*5*5*5
+        # test_4 all same input size conv3d
+        self.conv3d1 = nn.Conv3d(in_channels=1, out_channels=4, kernel_size=9, stride=1, padding=4)
         # 4, 25, 25, 25
-        self.conv3d2 = nn.Conv3d(4, 2, 7, stride=2, padding=3)
+        self.conv3d2 = nn.Conv3d(4, 2, 7, stride=1, padding=3)
         # 2, 13, 13, 13
-        self.conv3d3 = nn.Conv3d(2, 2, 3, stride=3, padding=2)
-
-        self.fltt = 2*5*5*5
-
-        self.mu1 = nn.Linear(self.fltt, 100)
-        self.mu2 = nn.Linear(100, a_dim)
-        self.sigma1 = nn.Linear(self.fltt, 100)
-        self.sigma2 = nn.Linear(100, a_dim)
+        self.conv3d_3mu = nn.Conv3d(2, 1, 3, stride=1, padding=1)
+        self.conv3d_3sigma = nn.Conv3d(2, 1, 3, stride=1, padding=1)
+        self.conv3d_3v = nn.Conv3d(2, 1, 3, stride=1, padding=1)
+        self.fltt = 1*50*50*50
+        # self.mu1 = nn.Linear(self.fltt, 100)
+        # self.mu2 = nn.Linear(100, a_dim)
+        # self.sigma1 = nn.Linear(self.fltt, 100)
+        # self.sigma2 = nn.Linear(100, a_dim)
         # self.c1 = nn.Linear(s_dim, 100)
         self.v1 = nn.Linear(self.fltt, 100)
         self.v2 = nn.Linear(100, 1)
-        set_init([self.conv3d1, self.conv3d2, self.conv3d3, self.mu1, self.mu2, self.sigma1, self.sigma2, self.v1, self.v2])
+        # set_init([self.conv3d1, self.conv3d2, self.conv3d3, self.mu1, self.mu2, self.sigma1, self.sigma2, self.v1, self.v2])
+        set_init([self.conv3d1, self.conv3d2, self.conv3d_3mu, self.conv3d_3sigma, self.conv3d_3v, self.v1, self.v2])
         self.distribution = torch.distributions.Normal
 
     def forward(self, x):
         x = F.relu(self.conv3d1(x))
-        x = F.max_pool3d(x, kernel_size=3, stride=1, padding=1)
+        # x = F.max_pool3d(x, kernel_size=3, stride=1, padding=1)
         x = F.relu(self.conv3d2(x))
-        x = F.max_pool3d(x, kernel_size=3, stride=1, padding=1)
-        x = F.relu(self.conv3d3(x))
-        x = F.max_pool3d(x, kernel_size=3, stride=1, padding=1)
-        x = torch.flatten(x, start_dim=1)
-        mu = F.relu(self.mu1(x))
-        mu = HIGH_A * torch.sigmoid(self.mu2(mu))
-        sigma = F.relu(self.sigma1(x))
-        sigma = F.softplus(self.sigma2(sigma)) + 0.001      # avoid 0
+        # x = F.max_pool3d(x, kernel_size=3, stride=1, padding=1)
+        mu = HIGH_A * torch.sigmoid(self.conv3d_3mu(x))
+        mu = mu.view(-1).softmax(0).view(*mu.shape)
+        sigma = F.softplus(self.conv3d_3sigma(x))
+        # x = F.max_pool3d(x, kernel_size=3, stride=1, padding=1)
+        # x = torch.flatten(x, start_dim=1)
+        # mu = F.relu(self.mu1(x))
+        # mu = HIGH_A * torch.sigmoid(self.mu2(mu))
+        # sigma = F.relu(self.sigma1(x))
+        # sigma = F.softplus(self.sigma2(sigma)) + 0.001      # avoid 0
+        x = torch.flatten(self.conv3d_3v(x), start_dim=1)
         x = F.relu(self.v1(x))
         values = self.v2(x)
         return mu, sigma, values
@@ -81,7 +93,8 @@ class Net(nn.Module):
     def choose_action(self, s):
         self.training = False
         mu, sigma, _ = self.forward(s)
-        m = self.distribution(mu.view(3, ).data, sigma.view(3, ).data)
+        m = self.distribution(mu, sigma)
+        # m = self.distribution(mu.view(3, ).data, sigma.view(3, ).data)
         return m.sample().numpy()
 
     def loss_func(self, s, a, v_t):
@@ -89,13 +102,19 @@ class Net(nn.Module):
         mu, sigma, values = self.forward(s)
         td = v_t - values
         c_loss = td.pow(2)
-
         m = self.distribution(mu, sigma)
         log_prob = m.log_prob(a)
         entropy = 0.5 + 0.5 * math.log(2 * math.pi) + torch.log(m.scale)  # exploration
-        exp_v = log_prob * td.detach() + 0.005 * entropy
+        # https://blog.csdn.net/weixin_43116379/article/details/90674462
+        # in xiaotong's understanding: under specific td, try to max the log_prob of a by changing the mu and sigma
+        for i in range(td.detach().shape[0]):
+            log_prob[i] *= td.detach()[i]
+        # exp_v = log_prob * td.detach() + 0.005 * entropy
+        exp_v = log_prob + 0.005 * entropy
         a_loss = -exp_v
-        total_loss = (a_loss + c_loss).mean()
+        total_loss = 0
+        for i in range(c_loss.shape[0]):
+            total_loss += (a_loss[i] + c_loss[i]).mean()
         return total_loss
 
 
