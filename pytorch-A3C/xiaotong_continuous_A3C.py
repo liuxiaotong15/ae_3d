12c76f1d936278700ebe69f8abf0bd7ea68e8e0c
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-04-27 15:04:16 +0800

    delete DIYed loss
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- pytorch-A3C/xiaotong_continuous_A3C.py
+++ pytorch-A3C/xiaotong_continuous_A3C.py
@@ -126,15 +126,15 @@ class Net(nn.Module):
         a_loss = -exp_v
         total_loss = (a_loss + c_loss).mean()
         
-        for s_idx in range(s.shape[0]):
-            for a_idx in range(a.shape[1]):
-                weight = 1.0
-                if(a_idx < a.shape[1] - 1):
-                    weight = 0.1
-                if a[s_idx][a_idx] > torch.max(s[s_idx][a_idx]):
-                    total_loss += ((a[s_idx][a_idx] - torch.max(s[s_idx][a_idx]))/s.shape[0] * weight)
-                if a[s_idx][a_idx] < torch.min(s[s_idx][a_idx]):
-                    total_loss += ((torch.min(s[s_idx][a_idx]) - a[s_idx][a_idx])/s.shape[0] * weight)
+        # for s_idx in range(s.shape[0]):
+        #     for a_idx in range(a.shape[1]):
+        #         weight = 1.0
+        #         if(a_idx < a.shape[1] - 1):
+        #             weight = 0.1
+        #         if a[s_idx][a_idx] > torch.max(s[s_idx][a_idx]):
+        #             total_loss += ((a[s_idx][a_idx] - torch.max(s[s_idx][a_idx]))/s.shape[0] * weight)
+        #         if a[s_idx][a_idx] < torch.min(s[s_idx][a_idx]):
+        #             total_loss += ((torch.min(s[s_idx][a_idx]) - a[s_idx][a_idx])/s.shape[0] * weight)
         return total_loss
 
 
