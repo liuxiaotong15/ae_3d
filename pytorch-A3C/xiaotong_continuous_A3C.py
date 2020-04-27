9e5205cc415905845b51d2e3dc0016a28dc2786c
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-04-27 09:11:42 +0800

    fix a batch bug
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- pytorch-A3C/xiaotong_continuous_A3C.py
+++ pytorch-A3C/xiaotong_continuous_A3C.py
@@ -125,14 +125,13 @@ class Net(nn.Module):
         exp_v = log_prob * td.detach() + 0.003 * entropy
         a_loss = -exp_v
         total_loss = (a_loss + c_loss).mean()
-        if s.shape[0] != 1:
-            print('must handle batch input...')
-            1/0
-        for a_idx in range(a.shape[1]):
-            if a[0][a_idx] > torch.max(s[0][a_idx]):
-                total_loss += (a[0][a_idx] - torch.max(s[0][a_idx]))
-            if a[0][a_idx] < torch.min(s[0][a_idx]):
-                total_loss += (torch.min(s[0][a_idx]) - a[0][a_idx])
+        
+        for s_idx in range(s.shape[0]):
+            for a_idx in range(a.shape[1]):
+                if a[s_idx][a_idx] > torch.max(s[s_idx][a_idx]):
+                    total_loss += ((a[s_idx][a_idx] - torch.max(s[s_idx][a_idx]))/s.shape[0])
+                if a[s_idx][a_idx] < torch.min(s[s_idx][a_idx]):
+                    total_loss += ((torch.min(s[s_idx][a_idx]) - a[s_idx][a_idx])/s.shape[0])
         return total_loss
 
 
