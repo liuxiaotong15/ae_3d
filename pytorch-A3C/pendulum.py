e3ea3c400d31894155ee48cc84a92a236c4eefdd
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-04-28 11:02:21 +0800

    amend
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- pytorch-A3C/pendulum.py
+++ pytorch-A3C/pendulum.py
@@ -70,7 +70,8 @@ class PendulumEnv(gym.Env):
             volume[1][i][j][k] = np.amax(np.array(dis_lst))
             volume[2][i][j][k] = np.amin(np.array(dis_lst))
         volume[-1] /= np.amax(volume[-1])
-        volume[0] /= np.amax(volume[0])
+        if np.amax(volume[0]) > 0:
+            volume[0] /= np.amax(volume[0])
         # print('max: ', np.amax(volume), 'min: ', np.amin(volume), 'mean: ', np.average(volume), 'atoms cnt: ', len(at))
         return volume
 
