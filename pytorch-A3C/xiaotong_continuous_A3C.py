876a6137812cbf3b21a1bb21a322d1ef2e963f67
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-05-28 20:45:14 +0800

    amend
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- pytorch-A3C/xiaotong_continuous_A3C.py
+++ pytorch-A3C/xiaotong_continuous_A3C.py
@@ -186,7 +186,7 @@ class Worker(mp.Process):
                 v1 = a[1] * (t+1)
                 v2 = a[2] * (t+1)
                 # v3 = a[3] * (t+1)
-                v = v3
+                v = a[-1] * (t+1)
                 xyz = np.array([0, 0, 0])
                 if v>=0:
                     # s1 = np.power(s[0], 2)
