5b630d57cb13e3a5adcb1ff49b78e204451efe2c
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-05-29 15:25:43 +0800

    mask count
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- pytorch-A3C/xiaotong_continuous_A3C.py
+++ pytorch-A3C/xiaotong_continuous_A3C.py
@@ -200,6 +200,7 @@ class Worker(mp.Process):
                     s2 = np.power(s[2] - v2, 2)
                     # s3 = np.power(s[3] - v3, 2)
                     ma1 = ma.masked_array(s1, s0>0.0001)
+                    print('False in masked_array: ', np.sum(ma1.mask==False))
                     # ma2 = ma.masked_array(s2, ma1.filled()>0.01)
                     # ma3 = ma.masked_array(s3, ma2.filled()>0.01)
                     # result1 = ma.where(s3 == ma3.filled().min())
