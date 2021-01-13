3eb87093c8aa462438413de4537e033a7c354cdd
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-01-13 22:18:15 +0800

    xyz 10 cpus is enough
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- pytorch-A3C/xyz_a3c.py
+++ pytorch-A3C/xyz_a3c.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     global_max_ep_r = mp.Value('d', 0.)
     # parallel training
     # workers = [Worker(gnet, opt, global_ep, global_ep_r, res_queue, i) for i in range(mp.cpu_count()-2)]
-    workers = [Worker(gnet, opt, global_ep, global_ep_r, global_max_ep_r, res_queue, i) for i in range(20)]
+    workers = [Worker(gnet, opt, global_ep, global_ep_r, global_max_ep_r, res_queue, i) for i in range(10)]
     [w.start() for w in workers]
     res = []                    # record episode reward to plot
     while True:
